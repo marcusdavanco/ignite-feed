0d399278cbd5fc2096dac508edbbdc49cf5440a2
Marcus Davanço <md.daves@gmail.com>
2023-03-10 17:48:47 -0300

    feat: propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import { Post } from "./Post";
 function App() {
   return (
     <div>
-      <Post />
+      <Post
+        author="Marcus Davanço"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor leo a diam sollicitudin. Mauris augue neque gravida in fermentum et."
+      />
+
+      <Post
+        author="Arthur Carvalho"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor leo a diam sollicitudin. Mauris augue neque gravida in fermentum et."
+      />
       <Post />
       <Post />
       <Post />
