4d5d2ed73660c216e103a9f8bb6dd01e5d55ed29
Marcus Davanço <md.daves@gmail.com>
2023-03-13 19:48:14 -0300

    feat: Componente: Avatar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { Pen } from "phosphor-react";
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -11,10 +12,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1579403124614-197f69d8187b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/marcusdavanco.png"
-        />
+        <Avatar src="https://github.com/marcusdavanco.png" />
         <strong>Marcus Davanco</strong>
         <span>Web Developer</span>
       </div>
