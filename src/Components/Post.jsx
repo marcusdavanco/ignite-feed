1d3117f37e3ba4d2a58db37c7756a269255c4ef8
Marcus Davanço <md.daves@gmail.com>
2023-03-13 16:46:31 -0300

    feat: Formulário de comentários

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -34,6 +34,13 @@ export function Post() {
           <a>#novoprojeto</a> <a>#nlw</a> <a>#rocketseat</a>
         </p>
       </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
