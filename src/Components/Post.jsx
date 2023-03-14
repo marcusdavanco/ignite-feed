10e0e6880f1ce9e61f63687880e855ef0550d336
Marcus Davanço <md.daves@gmail.com>
2023-03-14 19:05:48 -0300

    feat: Entendendo a key

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -57,10 +57,10 @@ export function Post({ author, content, publishedAt }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
