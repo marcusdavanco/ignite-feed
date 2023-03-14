0946fcee0bba164d80945541380130c4aef59fcc
Marcus Davanço <md.daves@gmail.com>
2023-03-14 19:18:43 -0300

    feat: Comunicação entre componentes

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -37,6 +37,10 @@ export function Post({ author, content, publishedAt }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(comment) {
+    console.log(`Deletar comentário ${comment}`);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -81,7 +85,13 @@ export function Post({ author, content, publishedAt }) {
       </form>
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment key={comment} content={comment} />;
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
