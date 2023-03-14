891ce60897c2a650caa99e9b56698550b7e164d9
Marcus Davanço <md.daves@gmail.com>
2023-03-14 17:26:35 -0300

    feat: feat: Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,51 @@ import { Post } from "./Components/Post";
 import { Sidebar } from "./Components/Sidebar";
 import "./global.css";
 
+/* author: { avatar_url: "", name: '', role: "" }
+ * publishedAt: Date
+ * content: String
+ */
+
+// TODO: Replace for something more realistic
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://gitub.com/marcusdavanco.png",
+      name: "Marcus Davanco",
+      role: "CEO @ Serendip Software",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "👉 jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-02-15 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://gitub.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @ Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "👉 jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-02-25 20:00:00"),
+  },
+];
+
 import styles from "./App.module.css";
 
 function App() {
@@ -12,9 +57,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
